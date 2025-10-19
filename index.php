1019f9a2e2c4107f0c1b514d61e5b523957619c2
AnissDa <aniss.dahaoui@hotmail.com>
2025-10-19 22:27:48 +0200

    creationog generic footer.php for further use

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 <body>
   <header class="site-header">
 
-    <?php include "partial/navbar.php"; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/partial/navbar.php"; ?>
   </header>
 
 
@@ -117,15 +117,7 @@
 
 <script src="js/app.js"></script>
 
+  <?php include $_SERVER['DOCUMENT_ROOT']."/partial/footer.php"; ?>
 
-<footer class="site-footer">
-
-  <p>© 2025 Aniss Dah. Tous droits réservés.</p>
-  <nav aria-label="Navigation secondaire">
-    <a href="/Politique%20de%20confidentialit%C3%A9/" class="footer-link">Politique de confidentialité</a>
-    <a href="/contact/" class="footer-link">Contact</a>
-  </nav>
-
-</footer>
 </body>
 </html>
---
+++ partial/footer.php
@@ -0,0 +1,9 @@
+<footer class="site-footer">
+
+  <p>© 2025 Aniss Dah. Tous droits réservés.</p>
+  <nav aria-label="Navigation secondaire">
+    <a href="/Politique%20de%20confidentialit%C3%A9/" class="footer-link">Politique de confidentialité</a>
+    <a href="/contact/" class="footer-link">Contact</a>
+  </nav>
+
+</footer>
