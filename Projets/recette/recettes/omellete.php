2d41f13de3a4998645b1fceeee168315aa2f6301
AnissDa <aniss.dahaoui@hotmail.com>
2025-10-20 23:30:04 +0200

    quelques ajouts de page avant la full migration de sports et recette dans projets

--- Projets/recette/recettes/omellete.php
+++ Projets/recette/recettes/omellete.php
@@ -22,13 +22,13 @@
 </head>
 <section>
 
-    <!-- header navbar -->
-    <?php include $_SERVER['DOCUMENT_ROOT']."/partial/navbar.php"; ?>
-    <!-- end header navbar -->
+  <!-- header navbar -->
+
+  <!-- end header navbar -->
 
 </section>
 <section>
-  <h1>MasterchefLab</h1>
+  <h1><a href="/Projets/">Master Chef</a></h1>
   <h2>Petite presentation</h2>
   <p>Ce site de recette a pour vocation de vous proposer un large pannelle de recette avec des alternative vegan,sans gluten, halal etc. des que cela est possible</p>
   <p>Merci de votre visite</p>
@@ -42,7 +42,7 @@
   </ul>
 </section>
 <section>
-<p> cette section est en cours de reflexion de mise en page merci de patienter</p>
+  <p> cette section est en cours de reflexion de mise en page merci de patienter</p>
 </section>
 <body>
 <!-- Footer -->
