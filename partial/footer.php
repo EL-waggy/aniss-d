2814b58f676a650ed3469e6640418d21492df5e8
AnissDa <aniss.dahaoui@hotmail.com>
2025-10-22 10:03:56 +0200

    | dans le footer 2

--- partial/footer.php
+++ partial/footer.php
@@ -2,9 +2,10 @@
 
 <footer class="site-footer">
 
-  <p>© 2025 Aniss Dah. Tous droits réservés.</p>
+
   <nav aria-label="Navigation secondaire">
-    <p><a href="/Politique%20de%20confidentialit%C3%A9/" class="footer-link">Politique de confidentialité</a>
+    <p>© 2025 Aniss Dah. Tous droits réservés.<br>
+      <a href="/Politique%20de%20confidentialit%C3%A9/" class="footer-link">Politique de confidentialité</a>
     |<a href="/contact/" class="footer-link">  Contact</a></p>
   </nav>
 
